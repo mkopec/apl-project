101476e7f35e4edbfa1a82eed2adb750665993cc
Michał Kopeć <mkopec12@gmail.com>
2015-12-06 00:59:09 +0100

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $(document).scroll(function() {
+        var yOffset = window.pageYOffset;
+        var navbarPosition = $('.header').offset().top + $('.header').height();
+
+        if (yOffset >= navbarPosition - 5) {
+            $('.navbar').addClass('sticky');
+            $('.header').css("margin-bottom", $('.navbar').height() + "px");
+        }
+
+        else {
+            $('.navbar').removeClass('sticky');
+            $('.header').css("margin-bottom", "0");
+        }
+    });
+});
